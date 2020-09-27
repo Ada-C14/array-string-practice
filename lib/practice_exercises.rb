324fd4e41025b4810252909147a8198699b0aa96
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-27 14:02:09 -0700

    add is_palindrome and longest_prefix solution

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,28 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n^2), because we have an array of chars, and we store the char counts in a hash.
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  chars = string.downcase.scan(/\w/)
+  char_counts = Hash.new(0)
+
+  chars.each do |char|
+    char_counts[char] += 1
+  end
+
+  return char_counts.values.all? { |chars| chars.even? } || char_counts.values.one? { |chars| chars.odd? }
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  prefix = strings[0]
+
+  strings[1..-1].each do |word|
+    while word.index(prefix) != 0
+      prefix = prefix[0...-1]
+    end
+  end
+
+  return prefix
 end
 
