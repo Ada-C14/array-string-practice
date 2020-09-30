f10c539799ba3299087067e1c6696147b97678e7
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-09-29 20:37:07 -0700

    attempted both methods

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,32 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string.downcase.gsub!(/[^a-z0-9\s]/i, '')   #removes all non-alphanumeric chars
+  string.gsub!(/\s+/, '')    #removes spaces
+
+  (string.size/2).times.all? { |i| string[i] == string[-i-1] }
+  # if list.length == 0
+  #   return 0
+  # end
+  # unique_items = 1
+  # list.length.times do |index|
+  #   if index != 0 && list[index - 1] != list[index]
+  #     unique_items +=1
+  #   end
+  # end
+  # return unique_items
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Did not have time to complete all the way
+
+# Time Complexity: O(n^2)
+# Space Complexity: O(n) 
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  prefix_string = ""
+  strings.first.length.times do |index|
+    string.each do |string|
+      string[index]
+    end
+    return prefix_string
+  end
 end
-
