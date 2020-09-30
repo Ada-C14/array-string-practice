b50ff220d19d5159affedee35e53934bc1c869db
Ren Carothers <rencarothers@gmail.com>
2020-09-29 23:17:00 -0700

    first stab at palindrome, passes tests, uses strings, regex, loops

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,8 +1,20 @@
+# sources:
+# https://stackoverflow.com/questions/17689155/how-to-ignore-nonword-charcters-in-ruby
 
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string = string.gsub(/\W/, "").downcase
+  x = 0
+  i = (string.length - 1)
+  until x >= i
+      if string[x] != string[i]
+        return false
+      end
+        x += 1
+        i -= 1
+  end
+  return true
 end
 
 # Time Complexity: ?
@@ -10,4 +22,3 @@ end
 def longest_prefix(strings)
   raise NotImplementedError, "Not implemented yet"
 end
-
