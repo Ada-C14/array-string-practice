e84d9134d744e27cfeb16c498071d0cb77c74f41
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-09-29 23:02:54 -0700

    revise complexity explanatinos

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,10 +1,13 @@
 
-# Time Complexity: ?   O(n): one loop, so the runtime will be affected by the length of the string.
-# Space Complexity: ?  O(1): it takes a fixed amount of space that is not depend on the input.
+# Time Complexity: ?   O(n): two loops, but the relationship between these two loops is O(n) + O(n/2)
+#                            so O(1.5n) --> O(n).
+# Space Complexity: ?  O(n): the amount of space is depend on the length of input String in the first loop
+#                            plus extra index variable.
 def is_palindrome(string)
   # raise NotImplementedError, "Not implemented yet"
   return false if string.nil? 
     string = string.downcase.split(//).select {|char| (/[a-z0-9]/).match? (char)}.join
+
     index = 0
     while index <= (string.length / 2)
         return false if string[index] != string[string.length - 1 - index]
@@ -13,12 +16,17 @@ def is_palindrome(string)
     return true
 end
 
-# Time Complexity: ?    O(n): two loops, but the second loop is a fixed number, we could say the runtime would be affected mainly by the length of the array. 
-# Space Complexity: ?   O(1): it takes a fixed amount of space that is not depend on the input. 
+# Time Complexity: ?    O(n*m): two loops, and the second loop is a nested loop.
+#                       the relationship between these two loops is O(n) + O(n*m), 
+#                       ie O(n): the length of the input array, O(m): the length of "shortest_word" in the input array,
+#                       so O((m+1)n) --> O(n*m).
+# Space Complexity: ?   O(n): the amount of space is depend on the length of "shortest_word" in the input array
+#                       plus extra index variable.
 def longest_prefix(strings)
   # raise NotImplementedError, "Not implemented yet"
   return "" if strings.nil? || strings.empty?
     shortest_word_for_longest_prefix = strings.min_by {|word| word.length }
+
     strings.each do |word|
         prefix = String.new
         index = 0
