25f52dc009e97e196602ffe9386ce175652ec527
Stacy <lundquist.stacy@gmail.com>
2020-10-02 13:11:21 -0700

    freakin finally! that last test re:punctuation was throwing me

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,18 +1,28 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
+
   return true if string.length == 0 || string.length == 1
-  string =~ /[0-9a-zA-z]/
+
+  string_values = /[0-9a-zA-z]/
+
 
   i = 0
   j = string.length - 1
   while i < j
-    return false if string[i].upcase != string[j].upcase
-    i += 1
-    j -= 1
+    if string_values.match(string[i]) && !string_values.match(string[j])
+      j -= 1
+    elsif !string_values.match(string[i]) && string_values.match(string[j])
+      i += 1
+    else
+      return false if string[i].upcase != string[j].upcase
+      i += 1
+      j -= 1
+    end
   end
 
   return true
+
 end
 
 # Time Complexity: ?
