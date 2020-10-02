7f3084e99438b12509ef4ba807137b2ccd6db263
marks214 <aimee.oz.x@gmail.com>
2020-10-02 12:04:38 -0700

    Wave 1 - is_palindrome method passing tests

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,8 +1,23 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string.downcase!
+  string_array = string.split("")
+  allowed_characters = ("a".."z").to_a
+  string_array.select! { |letter| allowed_characters.include?(letter) }
+  i = 0
+
+  while i <= string_array.length && !string_array[i].nil?
+    return false if string_array.first != string_array.last
+
+    string_array.pop
+    string_array.shift
+    i += 1
+  end
+
+  return true
+
 end
 
 # Time Complexity: ?
