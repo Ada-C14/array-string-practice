08c13bcb1941c058bdd0e96a461e5df91b2747ef
Tram Bui <pbui17@cmc.edu>
2020-10-03 16:16:35 -0500

    Answered the time and space complexities questions

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,17 +1,13 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(mn), the alt_string will be O(n) and the reverse_string method is
+# O(n). Alt_string is O(m). But since it's not nested, it'll be O(mn)
+# Space Complexity: O(n), reverse_string made an array to hold the string characters
 def is_palindrome(string)
-  # return true if string is empty
-  # else, turn string into characters array string.downcase.gsub(/[^a-z0-9]/,'')
-  # reverse the array
-  # check if the characters array is the same as the reversed array
-
-  alt_string = string.downcase.gsub(/[^a-z0-9]/,'')
+  alt_string = string.downcase.gsub(/[^a-z0-9]/,'') # elimate punctuation and spaces O(m)
 
   if string.empty?
     return true
-  elsif alt_string == reverse_string(alt_string)
+  elsif alt_string == reverse_string(alt_string) # O(n)
     return true
   else
     false
@@ -22,7 +18,7 @@ end
 def reverse_string(string)
   i = 0
   j = string.size - 1 # last letter in the string
-  string_arr = string.chars
+  string_arr = string.chars # O(n)
 
   while i < j
     temp = string_arr[i]
@@ -36,15 +32,15 @@ def reverse_string(string)
   return string_arr.join
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def longest_prefix(strings)
 
   result_string = ""
   next_word = 1
 
-  strings.first.each_char.with_index do |letter, i|
-      while next_word < strings.size
+  strings.first.each_char.with_index do |letter, i| # O(n)
+      while next_word < strings.size # O(n)
         if letter != strings[next_word][i]
           return result_string
         else
