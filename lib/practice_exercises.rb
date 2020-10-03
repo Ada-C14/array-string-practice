94dfee2ab68e20ae5ef281b016718cd2e6761194
Tram Bui <pbui17@cmc.edu>
2020-10-03 16:09:27 -0500

    completed is_palindrome?

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,7 +2,38 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
+  # return true if string is empty
+  # else, turn string into characters array string.downcase.gsub(/[^a-z0-9]/,'')
+  # reverse the array
+  # check if the characters array is the same as the reversed array
 
+  alt_string = string.downcase.gsub(/[^a-z0-9]/,'')
+
+  if string.empty?
+    return true
+  elsif alt_string == reverse_string(alt_string)
+    return true
+  else
+    false
+  end
+
+end
+
+def reverse_string(string)
+  i = 0
+  j = string.size - 1 # last letter in the string
+  string_arr = string.chars
+
+  while i < j
+    temp = string_arr[i]
+    string_arr[i] = string_arr[j]
+    string_arr[j] = temp
+
+    i += 1
+    j -= 1
+  end
+
+  return string_arr.join
 end
 
 # Time Complexity: ?
