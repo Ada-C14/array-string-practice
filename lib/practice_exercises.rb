d2959d1236cf85c84f23969972d1b439b7277172
Tram Bui <pbui17@cmc.edu>
2020-10-02 22:32:16 -0500

    finished and tested the method for longest_prefix

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,12 +2,29 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+
+  result_string = ""
+  next_word = 1
+
+  strings.first.each_char.with_index do |letter, i|
+      while next_word < strings.size
+        if letter != strings[next_word][i]
+          return result_string
+        else
+          next_word += 1
+        end
+      end
+      i += 1
+      result_string << letter
+      next_word = 1
+    end
+    return result_string
+
 end
 
