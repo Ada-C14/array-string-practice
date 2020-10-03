62de6ec8b2468c8b70dd985707e8892fe472fc3b
Jasmine <jasylop@gmail.com>
2020-10-03 15:11:37 -0700

    wrote methods for is_palindrome & longest_prefix

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,34 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# I have a loop that's dependant on the length of the string.
+# Space Complexity: O(n) because I'm creating a new string based on the input.
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string = string.gsub(/[^0-9a-zA-Z]/, '').downcase
+  first = 0
+  last = string.length - 1
+  until first >= last
+    return false if string[first] != string[last]
+    first += 1
+    last -= 1
+  end
+  return true
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n * m) where n is the length of an array and m is the length of a given string
+# worst case would be O(n^2), however if n becomes sufficiently large compared to m
+# O(n) would be more realistic.
+# Space Complexity: O(1), not creating new structures, just using variables & integers.
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  return "" if strings.nil? || strings.empty?
+
+  (strings[0].length).times do |index|
+    char = strings[0][index]
+    strings.each do |string|
+      if index == string.length || string[index] != char
+        return string[0...index]
+      end
+    end
+  end
+  return strings[0]
 end
 
