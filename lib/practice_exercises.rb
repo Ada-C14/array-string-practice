6e2f80e3995088a28ae10e5bba78f06e2a53c734
roshni-patel <rpatel01@wesleyan.edu>
2020-10-04 15:49:55 -0500

    Wrote is_palindrome and longest_prefix methods, all tests passed

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,41 @@
+# Time Complexity:
+# Space Complexity:
 
-# Time Complexity: ?
-# Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  reverse = ""
+  string.downcase!
+  string.gsub!(/[^0-9a-zA-Z]/, '')
+
+  if string.length <= 1
+    return true
+  else
+    first = 0
+    last = string.length - 1
+
+    while first <= last
+      temp = string[last]
+      reverse += temp
+      last -= 1
+    end
+
+    if reverse == string
+      return true
+    else
+      return false
+    end
+  end
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+
+# Time Complexity:
+# Space Complexity:
+
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  prefix = ""
+  strings[0].each_char.with_index do |letter, index|
+    strings.each do |string|
+      return prefix if letter != string[index]
+    end
+    prefix += letter
+  end
 end
-
