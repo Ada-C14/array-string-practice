0a72e4efe1d902f993309908bb30b7cd9a20ffe4
r-spiel <richelle.mehlhaff@gmail.com>
2020-10-03 19:11:55 -0700

    wave 1 - Valid palindrome

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,23 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string.downcase! #cannot have this in front of gsub or it will replace characters that are already downcase with nil
+  string.gsub!(/[^A-Za-z]/, '') #substitutes any non-letter characters with empty string
+
+  string.each_char.with_index do |letter, index|
+    if letter != string[string.length - 1 - index]
+      return false
+    end
+  end
+
+  return true
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
-end
 
+
+
+end
