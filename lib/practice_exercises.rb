a2aac3eebb7cb52ed66e53f25d388f82ead82d15
Kareha Agesa <kmaagesa@gmail.com>
2020-10-03 21:42:15 -0700

    is_palindrome working

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,8 +1,14 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string = string.downcase.gsub(/[^A-Za-z0-9]/, '')
+  length = string.length
+  (string.length/2).times do |i|
+    return false if string[i] != string[length-1]
+    length -= 1
+  end
+  return true
 end
 
 # Time Complexity: ?
