bd8858cf6057879a02a574a9c8d7233402a48e26
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-10-04 22:18:11 -0700

    both methods compelte

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -3,35 +3,27 @@
 
 def is_palindrome(string)
   string.downcase!
-  string.gsub!(/[^a-z0-9\s]/i, '')   #removes all non-alphanumeric chars
+  string.gsub!(/[^a-z0-9\s]/i, '')    #removes all non-alphanumeric chars
   string.gsub!(/\s+/, '')    #removes spaces
 
-  print string
-  (string.size/2).times.all? { |i| string[i] == string[-i-1] }
+  (string.size/2).times.all? do |index|
+    string[index] == string[-index-1]  #returns true or false
+  end
+
 end
 
-sentence = "Sore was I ere I saw Eros."
-print is_palindrome(sentence)
 
-# Time Complexity: O(n^2)
-# Space Complexity: O(n) 
-
-# def longest_prefix(strings)
-#   prefix_string = ""
-#   strings.first.length.times do |index|
-#     string.each do |string|
-#       string[index]
-#     end
-#     return prefix_string
-#   end
-# end
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 
 def longest_prefix(strings)
-  prefix_string = ""
-  strings.first.length.times do |index|
-    string.each do |string|
-      string[index]
-    end
-    return prefix_string
+  return '' if strings.empty?
+  min, max = strings.minmax #returns a new 2-element array containing the minimum & maximum length strings
+
+  idx = min.size.times do |index|  #assigns 'idx' the value of the index num of the letter in the min string that is the last to match the one at same index of in max
+    break index if min[index] != max[index]
   end
+
+  return min[0...idx]
 end
+
