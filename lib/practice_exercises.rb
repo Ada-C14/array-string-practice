96df906b289d0bb5ee1e414676bfe3627d65fdc0
Marj E <marj@marjorieetienne.com>
2020-10-05 08:42:43 -0700

    solved all but test 5

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -25,12 +25,35 @@ end
 # Space Complexity: ?
 # What if input is : ['flower', 'dog', 'flight']?
 def longest_prefix(strings)
-  # match = ""
-  # length = strings.length
-  # smallest_word = strings.min_by { |x| x.length }
-  #
-  #
+  match = ""
+  length = strings.length
+  smallest_word = strings.min_by { |x| x.length }
+  # puts "smallest word : #{smallest_word}"
+
+  # compare index of smallest word to the same index of each word in the array, if true return count, compare count to strings length, add to the match string, if false don't add
+  smallest_word.length.times do |letter_index|
+    count = 0
+    strings.each do |word|
+      next if word == smallest_word
+      # puts "word : #{word}", "word letter : #{word[letter_index]}"
+
+      if smallest_word[letter_index] == word[letter_index]
+        count += 1
+        # puts "count : #{count} / #{length}"
+      else
+        break
+      end
+
+    end
+
+    if count == length - 1
+      match << smallest_word[letter_index]
+    elsif strings.count(smallest_word) == length
+      match = smallest_word
+    end
+  end
 
   return match
 end
 
+# p longest_prefix(["carborator","carvonulted","carsome", "calsome"])
