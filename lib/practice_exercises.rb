9aac5cd3170ef289faf14760c8ced3256d94c4ce
Schanen Ryan <schanen.r@gmail.com>
2020-10-04 21:33:16 -0700

    method written, tests passing, space & time evaluated

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,59 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: ? O(n) The time is based on input and is linear.
+# Space Complexity: ? O(n) Still confused! But, the space use is not constant so it is O(n)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+
+  pattern = /[A-Z]/i
+
+  char_arr = string.split("")
+  alpha_arr = char_arr.select {|char| pattern =~ char}
+  total_char = alpha_arr.length
+
+  low = 0
+  high = total_char - 1
+
+  while low < high
+    if alpha_arr[low].casecmp?(alpha_arr[high])
+      low += 1
+      high -=1
+    else
+      return false
+    end
+  end
+
+  return true
+  # raise NotImplementedError, "Not implemented yet"
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: ? O(n^2) = O(n) + (O(n) * O(n))  because there is one nested loop of length 'n'
+# # Space Complexity: ? O(n) The space is determined by string.length and by control.length
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+
+  control = strings.min_by {|word| word.length }
+
+  strings.each do |string|
+    index = 0
+    prefix = ""
+
+    while index < control.length do
+      if control[index] == string[index]
+        prefix += control[index]
+        index += 1
+      else
+        break
+      end
+    end
+
+    return "" if control.empty?
+    control = prefix
+  end
+
+  return control
 end
+
+
+
+
+
+
 
