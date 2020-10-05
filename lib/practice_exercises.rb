55fbf209a149b4dea49d39524dcc1d50834f5e73
Alice Boone <alicegbonel@gmail.com>
2020-10-05 13:27:50 -0700

    Changes in the array-string-practice- Wave 1 and 2

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,28 @@
-
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: ? O(n)
+# Space Complexity: ? O(n)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+string = string.gsub(/[^0-9a-zA-Z]/, '').downcase
+  if string.length <= 1
+    true
+  else
+    if string[0] == string[-1]
+      is_palindrome(string[1..-2])
+    else
+      false
+    end
+  end
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: ? O(n+m)
+# Space Complexity: ? O(n)
 def longest_prefix(strings)
-  raise NotImplementedError, "Not implemented yet"
+  prefix = ""
+  i = 0
+  strings[0].each_char do |letter|
+    strings.each do |word|
+      return prefix unless word[i] == letter
+    end
+    prefix << letter
+    i += 1
+  end
 end
-
