0e3f90e63e8d1fb9cae47830b6cf5a60a85f92fe
Beatrice T <be@trice.codes>
2020-10-04 21:50:37 -0700

    Created is_palindrome method

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,8 +1,26 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  raise ArgumentError 'Input is not a string' unless string.class == String
+  return true if string == ""
+  string.upcase!
+
+  beg = 0
+  fin = string.length - 1
+
+  while beg < fin do
+    if string[beg] =~ /[a-z0-9]/i && string[fin] =~ /[a-z0-9]/i
+      return false if string[beg] != string[fin]
+      beg += 1
+      fin -= 1
+    else
+      beg += 1 unless string[beg] =~ /[a-z0-9]/i
+      fin -= 1 unless string[fin] =~ /[a-z0-9]/i
+    end
+  end
+
+  return true
 end
 
 # Time Complexity: ?
