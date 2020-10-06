bfd6e2295599397f198803fecbd1a558ce86b002
India Kato <india.kato@gmail.com>
2020-10-06 09:04:29 -0700

    works for one word strings

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,8 +2,17 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  string_array = string.scan /\w/
+
+  index_1 = 0
+  index_2 = string_array.length - 1
+
+  if string_array.length == 1
+    return true
+  end
+  
 end
+
 
 # Time Complexity: ?
 # Space Complexity: ?
