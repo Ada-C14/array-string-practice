65f35f4eb8e6ffe65e4cfb70309c5147a35da2a8
Lina Do <l.do5147@gmail.com>
2020-10-05 19:18:23 -0700

    added comments on time and space complexity for the is_panindrome method

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -1,13 +1,20 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: 0(n), where n is the length of the string
+# The loop in this method get ran 1/2n
+# dropping the coefficient will be O(n)
+# Space Complexity: 0(n), where n is the length of the string
+# a new string is created based off of the input string
 def is_palindrome(string)
   # replace all non letters with empty string
   new_string = string.upcase.gsub(/[^A-Za-z]/, '')
 
+  # a starts at index 0
+  # b is the last index of the string
   a = 0
   b = new_string.length - 1
 
+  # compares each letter starting at the ends of the string
+  # for every loop, a increases by one and b decreases by one
   while a <= b
     if new_string[a] != new_string[b]
       return false
