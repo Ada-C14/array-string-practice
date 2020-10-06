5fba0d489c5ef1197d21d0851f92d57bef06d748
Alice D <alicedilanchian@gmail.com>
2020-10-05 22:18:08 -0500

    Wave 1

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,7 +2,30 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  @string = string
+  # downcase  & strip all the nonalphanumeric characters, and save as a new variable @alphanumeric_only_string
+  @alphanumeric_only_string = @string.downcase.delete('^A-Za-z0-9')
+
+  # convert @alphanumeric_only_string to an array @alphanumeric_array
+  @alphanumeric_array = @alphanumeric_only_string.chars
+
+  #iterate through array and compare first & last, then repeat for next ones
+  i = 0
+  j = @alphanumeric_array.length - 1
+  sum_of_diff = 0
+  until i == @alphanumeric_array.length
+    if @alphanumeric_array[i] == @alphanumeric_array[j]
+      #do nothing
+    else
+      sum_of_diff += 1
+    end
+    i += 1
+    j -= 1
+  end
+
+  # returns true if the the sum_of_difference equals zero, false otherwise.
+  sum_of_diff == 0 ? true : false
+
 end
 
 # Time Complexity: ?
@@ -10,4 +33,3 @@ end
 def longest_prefix(strings)
   raise NotImplementedError, "Not implemented yet"
 end
-
