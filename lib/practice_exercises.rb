e856d9ce13e5c6c4b34428d05e945da8c3824e17
Lina Do <l.do5147@gmail.com>
2020-10-05 19:07:57 -0700

    completed the is_palindrome method to pass tests

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -2,7 +2,22 @@
 # Time Complexity: ?
 # Space Complexity: ?
 def is_palindrome(string)
-  raise NotImplementedError, "Not implemented yet"
+  # replace all non letters with empty string
+  new_string = string.upcase.gsub(/[^A-Za-z]/, '')
+
+  a = 0
+  b = new_string.length - 1
+
+  while a <= b
+    if new_string[a] != new_string[b]
+      return false
+    end
+
+    a += 1
+    b -= 1
+  end
+  # return true or false
+  return true
 end
 
 # Time Complexity: ?
@@ -10,4 +25,5 @@ end
 def longest_prefix(strings)
   raise NotImplementedError, "Not implemented yet"
 end
+
 
