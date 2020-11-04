561105a9e4b05c23c3f35da286c15b71f109fa72
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-03 20:45:12 -0800

    all waves passing

--- lib/practice_exercises.rb
+++ lib/practice_exercises.rb
@@ -7,18 +7,30 @@ def is_palindrome(string)
 
   i = 0
   while i < (reduced_string.length - 1)
-    if reduced_string[i] != reduced_string[reduced_string.length - 1]
+    if reduced_string[i] != reduced_string[reduced_string.length - i - 1]
       return false
     end
+    i += 1
   end
 
   return true
 end
 
 
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
+def longest_prefix(strings)
+  prefix = ""
+  i = 0
 
-# Time Complexity: ?
-# Space Complexity: ?
-# def longest_prefix(strings)
-#   raise NotImplementedError, "Not implemented yet"
-# end
+  return prefix if strings.empty?
+
+  strings[0].each_char do |letter|
+    strings.each do |word|
+      return prefix unless word[i] == letter
+    end
+    prefix << letter
+    i += 1
+  end
+
+end
